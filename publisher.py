3606ca6c0840e8f1e7724dc99afe8de174846044
Pozitivehero1 <pozitivehero@gmail.com>
2026-06-16 10:22:47 +0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -5,12 +5,12 @@ KEY = os.getenv("SQUARE_API")
 
 def publish(text):
 
-    requests.post(
-        "https://www.binance.com/bapi/composite/v1/public/pgc/openApi/content/add",
-        headers={
-            "X-Square-OpenAPI-Key": KEY,
-            "clienttype": "binanceSkill",
-            "Content-Type": "application/json"
-        },
-        json={"bodyTextOnly": text}
-    )
+    url = "https://www.binance.com/bapi/composite/v1/public/pgc/openApi/content/add"
+
+    requests.post(url, headers={
+        "X-Square-OpenAPI-Key": KEY,
+        "clienttype": "binanceSkill",
+        "Content-Type": "application/json"
+    }, json={
+        "bodyTextOnly": text
+    })
