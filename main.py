8a732e4d80a3e54f0150f7d0eedf5604b491d07a
Pozitivehero1 <pozitivehero@gmail.com>
2026-06-16 15:33:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,66 @@
+from data import get_data
+from indicators import build_indicators
+from filters import score_signal
+from writer import write_post
 from publisher import publish
+
+symbols = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "BNBUSDT",
+    "XRPUSDT",
+    "DOGEUSDT",
+    "PEPEUSDT",
+    "WIFUSDT"
+]
 
 print("BOT STARTED")
 
-text = """
-Тестовый пост.
+candidates = []
 
-Если вы видите этот пост, значит API публикации работает.
+for s in symbols:
 
-#BTC #Crypto
-"""
+    print(f"Analyzing {s}")
 
-response = publish(text)
+    raw = get_data(s)
+
+    if raw is None:
+        print(f"Skip {s}")
+        continue
+
+    d = build_indicators(raw)
+    d["symbol"] = s
+
+    score = score_signal(d)
+
+    print(f"{s} score = {score}")
+
+    if score >= 4:
+        d["score"] = score
+        candidates.append(d)
+
+print("Candidates:", len(candidates))
+
+if not candidates:
+    print("No good setups found")
+    exit()
+
+candidates = sorted(
+    candidates,
+    key=lambda x: x["score"],
+    reverse=True
+)
+
+best = candidates[0]
+
+print("Generating post for", best["symbol"])
+
+post = write_post(best)
+
+print("POST:")
+print(post)
+
+publish(post)
 
 print("DONE")
