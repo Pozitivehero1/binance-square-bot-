8542747954427e5179eaa39fcd1a3091d74c2f14
Pozitivehero1 <pozitivehero@gmail.com>
2026-06-16 10:25:54 +0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -7,10 +7,19 @@ def publish(text):
 
     url = "https://www.binance.com/bapi/composite/v1/public/pgc/openApi/content/add"
 
-    requests.post(url, headers={
+    payload = {
+        "bodyTextOnly": text
+    }
+
+    headers = {
         "X-Square-OpenAPI-Key": KEY,
         "clienttype": "binanceSkill",
         "Content-Type": "application/json"
-    }, json={
-        "bodyTextOnly": text
-    })
+    }
+
+    r = requests.post(url, headers=headers, json=payload)
+
+    print("STATUS:", r.status_code)
+    print("RESPONSE:", r.text)
+
+    return r.text
