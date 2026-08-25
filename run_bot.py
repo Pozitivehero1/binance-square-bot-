cb6f8ebd52456e365133862bbca1ea13f2266b01
Pozitivehero1 <pozitivehero@gmail.com>
2026-08-25 09:47:07 +0300

    v11.4.3: enable Language + Reach Guard

--- run_bot.py
+++ run_bot.py
@@ -13,11 +13,13 @@ from runtime_hotfix_v113 import apply_v113_hotfix
 from runtime_hotfix_v114 import apply_v114_hotfix
 from runtime_hotfix_v1141 import apply_v1141_hotfix
 from runtime_hotfix_v1142 import apply_v1142_hotfix
+from runtime_hotfix_v1143 import apply_v1143_hotfix
 
 apply_v113_hotfix()
 apply_v114_hotfix()
 apply_v1141_hotfix()
 apply_v1142_hotfix()
+apply_v1143_hotfix()
 
 from main import main
 
